2f6695f941095ca373446dc3f2959b94c48151b8
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2021-09-17 10:03:36 +0200

    validate comments

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -25,6 +25,43 @@ const Modal = () => {
         setModalActive(false);
     }
 
+    const resetInput = (e) => {
+        e.target.value = '';
+        e.target.style.borderColor = 'initial';
+
+        const error_message = document.querySelector('.error_message');
+        if(error_message) {
+            error_message.remove();
+        }
+    };
+
+    const handleError = (element, error_message) => {
+        element.style.borderColor = 'red';
+        const error = document.querySelector('.error_message');
+
+        if(!error) {
+            element.parentElement.insertAdjacentHTML('beforeend', `<p class="error_message">${error_message}</p>`);
+        }
+    };
+
+    const handleSubmit = () => {
+        const requiredInputs = [...document.querySelectorAll('.required')];
+
+        let hasError = false;
+
+        for (let input of requiredInputs) {
+            if(!input.value) {
+                hasError = true;
+                handleError(input, 'Du skal udfylde feltet');
+                return
+            }
+        }
+
+        if(!hasError) {
+            createReview();
+        }
+    }
+
     const createReview = async () => {
         const url = `https://api.mediehuset.net/homelands/reviews`;
         const formData = new FormData();
@@ -46,15 +83,15 @@ const Modal = () => {
     return (
         <div className={modalActive ? `${Style.modal} ${Style.active}` : `${Style.modal}`}>
             <form>
-                <input name="title" type="text" placeholder="Titel" value={title} onChange={(e) => setTitle(e.target.value)} />
-                <textarea name="content" placeholder="Kommentar" value={content} onChange={(e) => setContent(e.target.value)}>
+                <input className="required" name="title" type="text" placeholder="Titel" value={title} onFocus={(e) => resetInput(e)} onChange={(e) => setTitle(e.target.value)} />
+                <textarea className="required" name="content" placeholder="Kommentar" value={content} onFocus={(e) => resetInput(e)} onChange={(e) => setContent(e.target.value)}>
                 </textarea>
 
                 <RatingSystem setRating={setRating} rating={rating} />
 
                 <span>
                     <button type="button" onClick={closeModal}>Annuller</button>
-                    <button type="button" onClick={createReview}>Send</button>
+                    <button type="button" onClick={handleSubmit}>Send</button>
                 </span>
             </form>
         </div>
